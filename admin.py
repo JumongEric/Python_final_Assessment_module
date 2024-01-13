abd6bb38990d4a7da119164c7c383dd9c460a82a
Jumong <jumongericc@gmail.com>
2024-01-13 19:06:09 +0200

    Added new libraries

--- admin.py
+++ admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from lmsApp import models
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+
 
 # Register your models here.
-# admin.site.register(models.Groups)
+# admin.site.register(models.Groups 
